36113fd2413768520eb8a4bd3e086497d6274219
angelh187 <angelh187@nycstudents.net>
2026-02-10 10:00:42 -0500

    done

--- guessinggame.py
+++ guessinggame.py
@@ -1,6 +1,30 @@
-for i in range (1,11):
-    question = int(input("guess a number from 1-10"))
+
+import random
 
 
+""" list = [1,2,3,4,5,6,7,8,9,10] """
+""" x = int(input("guess a number from 1-10")) """
+""" num = random.choice(list) """
+""" while x != num: """
+
+
+guess_history = []
+number = random.randint(1,10)
+x = int(input("guess a number from 1-10"))
+
+
+while number != x:
+    guess_history.append(x)
+    if x > number:
+        print(guess_history)
+        print("your number is too big!")
+    if x < number:
+        print(guess_history)
+        print("your number is too small!")
+
+    x = int(input("guess again!"))
+
+print(guess_history)
+print("YAYAYYAYA CORRECT")
 
 
